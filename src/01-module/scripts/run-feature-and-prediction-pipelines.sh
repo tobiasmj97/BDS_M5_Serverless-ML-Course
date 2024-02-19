6544e9c8137b3df312076f7c2ef36b17b7810e91
RJurow <roman.j@me.com>
2024-02-19 16:23:35 +0100

    -

--- src/01-module/scripts/run-feature-and-prediction-pipelines.sh
+++ src/01-module/scripts/run-feature-and-prediction-pipelines.sh
@@ -5,11 +5,11 @@ set -e
 cd src/01-module
 
 # Run the feature pipeline
-python -m iris_feature_pipeline.py
+python iris-feature-pipeline.py
 
 # Run the batch inference pipeline
-python -m iris_batch_inference_pipeline.py
+python iris_batch_inference_pipeline.py
 
 # jupyter nbconvert --to notebook --execute iris-feature-pipeline.ipynb
 # jupyter nbconvert --to notebook --execute iris-batch-inference-pipeline.ipynb
-
+ 
